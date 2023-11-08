022ec3fa04a26ce81e7c7c150e9ae086d0afc060
Nono-Le-Robot <87859199+Nono-Le-Robot@users.noreply.github.com>
2023-11-08 18:36:51 +0100

    add speech

--- server.js
+++ server.js
@@ -3,19 +3,9 @@ const Gpio = require('onoff').Gpio;
 const app = express();
 const led = new Gpio(6, 'out'); // Utilisez le bon numéro de GPIO
 
-const annyang = require('annyang');
-annyang.start();
+const { initSpeechRecognition, startListening } = require('speech-recognition');
 
-annyang.addCommands({
-  'allume la led': () => {
-    led.writeSync(1); // Allumer la LED
-    console.log('LED allumée');
-  },
-  'éteins la led': () => {
-    led.writeSync(0); // Éteindre la LED
-    console.log('LED éteinte');
-  },
-});
+initSpeechRecognition();
 
 app.use(express.static('public')); // Dossier pour les fichiers statiques (HTML, CSS, JS)
 
@@ -28,6 +18,22 @@ app.get('/off', (req, res) => {
   led.writeSync(0); // Éteindre la LED
   res.send('LED éteinte');
 });
+
+// Écoute la commande vocale pour allumer la LED
+app.get('/listen', (req, res) => {
+  startListening((command) => {
+    if (command === 'allume la led') {
+      led.writeSync(1); // Allumer la LED
+      res.send('LED allumée');
+    } else if (command === 'éteins la led') {
+      led.writeSync(0); // Éteindre la LED
+      res.send('LED éteinte');
+    } else {
+      res.send('Commande vocale non reconnue');
+    }
+  });
+});
+
 
 const server = app.listen(3000, () => {
   console.log('Serveur démarré sur le port 3000');
