7b2f2abf93bcae8d2635e7acf39e58dadf36f013
Nono-Le-Robot <87859199+Nono-Le-Robot@users.noreply.github.com>
2023-11-08 15:22:06 +0100

    infinite blink

--- blink.js
+++ blink.js
@@ -1,7 +1,7 @@
 const Gpio = require('onoff').Gpio;
 const port_GPIO = 6;
 const LED = new Gpio(port_GPIO, 'out');
-const blinkInterval = setInterval(blinkLED, 250);
+const blinkInterval = setInterval(blinkLED, 1000);
 
 console.clear();
 console.log("========================================");
@@ -24,4 +24,4 @@ function endBlink() {
     console.log("========================================");
 }
 
-setTimeout(endBlink, 5000);
+// setTimeout(endBlink, 5000);
