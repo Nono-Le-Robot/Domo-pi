7155288f172cf0138f44f86c3f2e408ef3068b6a
Nono-Le-Robot <87859199+Nono-Le-Robot@users.noreply.github.com>
2023-11-08 17:51:37 +0100

    change frequzence

--- blink.js
+++ blink.js
@@ -1,7 +1,7 @@
 const Gpio = require('onoff').Gpio;
 const port_GPIO = 6;
 const LED = new Gpio(port_GPIO, 'out');
-const blinkInterval = setInterval(blinkLED, 1000);
+const blinkInterval = setInterval(blinkLED, 200);
 
 console.clear();
 console.log("========================================");
