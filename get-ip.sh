d21fdd1a8e7e7b9cdc4bcd37d95f60e7affbd112
Nono-Le-Robot <87859199+Nono-Le-Robot@users.noreply.github.com>
2023-11-16 12:42:46 +0100

    okokopojpm

--- get-ip.sh
+++ get-ip.sh
@@ -12,30 +12,34 @@ send_message() {
     -d text="$message"
 }
 
-# Tentatives de récupération de l'adresse IP (avec un maximum de 3 tentatives)
-for i in {1..3}; do
-  IP_ADDRESS=$(curl -s api64.ipify.org)
+while true; do
+  # Tentatives de récupération de l'adresse IP (avec un maximum de 3 tentatives)
+  for i in {1..3}; do
+    IP_ADDRESS=$(curl -s api64.ipify.org)
   
+    # Vérifier si l'adresse IP a été obtenue avec succès
+    if [ -n "$IP_ADDRESS" ]; then
+      break  # Sortir de la boucle si l'adresse IP a été obtenue avec succès
+    else
+      # Attendre 5 secondes avant de réessayer
+      sleep 5
+    fi
+  done
+
   # Vérifier si l'adresse IP a été obtenue avec succès
   if [ -n "$IP_ADDRESS" ]; then
-    break  # Sortir de la boucle si l'adresse IP a été obtenue avec succès
+    # Supprimer tous les messages précédents dans le chat
+    curl -s \
+      -X POST \
+      https://api.telegram.org/bot$BOT_TOKEN/deleteMessage \
+      -d chat_id=$CHAT_ID
+
+    # Envoyer le nouveau message avec l'adresse IP
+    send_message "IP Address: $IP_ADDRESS"
   else
-    # Attendre 5 secondes avant de réessayer
-    sleep 5
+    send_message "Erreur lors de la récupération de l'adresse IP."
   fi
+
+  # Attendre une minute avant la prochaine itération
+  sleep 60
 done
-
-# Vérifier si l'adresse IP a été obtenue avec succès
-if [ -n "$IP_ADDRESS" ]; then
-  # Supprimer tous les messages précédents dans le chat
-  curl -s \
-    -X POST \
-    https://api.telegram.org/bot$BOT_TOKEN/deleteMessage \
-    -d chat_id=$CHAT_ID
-
-  # Envoyer le nouveau message avec l'adresse IP
-  send_message "IP Address: $IP_ADDRESS"
-else
-  send_message "Erreur lors de la récupération de l'adresse IP."
-fi
-
--- start.sh
+++ start.sh
@@ -1,13 +1,18 @@
 tmux new-session -d -s mysession
 
+# Fenêtre 1
 tmux split-window -v -t mysession
-
 tmux select-pane -t 0
-
 tmux send-keys -t mysession:0.0 'sudo node server.js' C-m
 
+# Fenêtre 2
+tmux split-window -h -t mysession
 tmux select-pane -t 1
-
 tmux send-keys -t mysession:0.1 'sudo ./restart-lt.sh' C-m
+
+# Fenêtre 3
+tmux new-window -t mysession
+tmux select-pane -t 0
+tmux send-keys -t mysession:1.0 'sudo ./get_ip.sh' C-m
 
 tmux attach -t mysession
