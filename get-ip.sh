a74bdb10c0a883145597eccb66221842040464db
Nono-Le-Robot <87859199+Nono-Le-Robot@users.noreply.github.com>
2023-11-16 11:36:22 +0100

    with mess id

--- get-ip.sh
+++ get-ip.sh
@@ -1,15 +1,29 @@
 BOT_TOKEN="6334153050:AAGAd8dCN4Pw3zROFFO-dY-HhzS2890ef1M"
 CHAT_ID="2059864279"
+MESSAGE_ID="17"
 
-# Fonction pour envoyer le message à Telegram
-send_message() {
+# Fonction pour envoyer ou éditer le message à Telegram
+send_or_edit_message() {
   local message="$1"
 
-  curl -s \
-    -X POST \
-    https://api.telegram.org/bot$BOT_TOKEN/sendMessage \
-    -d chat_id=$CHAT_ID \
-    -d text="$message"
+  # Si MESSAGE_ID est vide, cela signifie que le message n'a pas encore été envoyé
+  if [ -z "$MESSAGE_ID" ]; then
+    # Envoyer le nouveau message avec l'adresse IP
+    MESSAGE_ID=$(curl -s \
+      -X POST \
+      https://api.telegram.org/bot$BOT_TOKEN/sendMessage \
+      -d chat_id=$CHAT_ID \
+      -d text="$message" \
+      | jq -r .result.message_id)
+  else
+    # Éditer le message existant avec la nouvelle adresse IP
+    curl -s \
+      -X POST \
+      https://api.telegram.org/bot$BOT_TOKEN/editMessageText \
+      -d chat_id=$CHAT_ID \
+      -d message_id=$MESSAGE_ID \
+      -d text="$message"
+  fi
 }
 
 # Tentatives de récupération de l'adresse IP (avec un maximum de 3 tentatives)
@@ -27,15 +41,8 @@ done
 
 # Vérifier si l'adresse IP a été obtenue avec succès
 if [ -n "$IP_ADDRESS" ]; then
-  # Supprimer tous les messages précédents dans le chat
-  curl -s \
-    -X POST \
-    https://api.telegram.org/bot$BOT_TOKEN/deleteMessage \
-    -d chat_id=$CHAT_ID
-
-  # Envoyer le nouveau message avec l'adresse IP
-  send_message "$IP_ADDRESS"
+  # Envoyer ou éditer le message avec l'adresse IP
+  send_or_edit_message "IP Address: $IP_ADDRESS"
 else
-  send_message "Erreur lors de la récupération de l'adresse IP."
+  send_or_edit_message "Erreur lors de la récupération de l'adresse IP."
 fi
-
